588daff8247075d2a77a85164f33bdff4a8b5bfb
DmytroLebediev <kinglearXIV@gmail.com>
2017-04-10 23:07:53 +0300

    Pointer initialization
    Initialize pointer in a function

---
+++ group3/week5/pointer_ex0.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void init_pointer(int* root);
+
+int main()
+{
+    int* root = NULL;
+    init_pointer(root);
+    
+    if (root != NULL)
+    {
+        printf("value[0]=%i\n", *root);
+    }
+    else
+    {
+        printf("nothing\n");
+    }
+}
+
+void init_pointer(int* root)
+{
+    root = malloc(1 * sizeof(int));
+    if (root != NULL)
+    {
+        *root = 10;
+    }
+}
